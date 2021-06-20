021168b487ac107ff6354d27e00b8c021d73d52e
vishalnair198 <84738211+vishalnair198@users.noreply.github.com>
2021-06-20 13:49:41 +0530

    Update settings.py

--- we/settings.py
+++ we/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=4^p%u)flxiy2s#32n&#764!8ot35%5@bd&4gkvyd^o+r5zpgp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'vishalnair198-testt-master.zeet.app']
 
 
 # Application definition
